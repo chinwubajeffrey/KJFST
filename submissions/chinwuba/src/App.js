ecd610bb60571c2dbd1e95f0aa1a00cb38cc7146
Chinwuba <chinwubajeffrey55@gmail.com>
2025-09-10 17:41:44 +0100

    first

---
+++ submissions/chinwuba/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import Card from '../components/Card'
+
+
+const itemsPerPage = 6;
+
+const App = () => {
+  const [topic, setTopic] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
+
